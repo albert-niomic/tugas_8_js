ff04c05710df6c265e828d8e39685eb6e409e602
Albert <akuanakrajin7@gmail.com>
2021-12-30 05:50:14 +0700

    Tugas 8 Java Script

---
+++ tugas8.js
@@ -0,0 +1,15 @@
+function TampilObject(){
+  var mahasiswa = {
+    nim : 62362,
+    umur : 22,
+    ipk : 3.9
+  };
+
+  mahasiswa.nim = 26673;
+  mahasiswa.umur = 21;
+  mahasiswa.ipk = 4;
+
+  return mahasiswa;
+}
+
+console.log(TampilObject());
